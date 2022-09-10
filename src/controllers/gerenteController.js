fbb0b350ffe9b9baba2c6252969598dd5d6486bd
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-09-10 16:12:09 -0300

    Implementação rotas e controllers de gerente

--- src/controllers/gerenteController.js
+++ src/controllers/gerenteController.js
@@ -18,7 +18,7 @@ module.exports = {
     async consultarClientes(req, res) {
     
         var urlConsultarClientes = `http://localhost:5001/listar`;
-    
+        
         await request(
           {
             url: urlConsultarClientes,
@@ -29,9 +29,35 @@ module.exports = {
           },
           function (error, response, body) {
             if (!error) {
-              console.log(body);
-              jsonBody = JSON.parse(body)
-              return res.status(response.statusCode).json(jsonBody);
+                console.log(body);
+                const clientes = JSON.parse(body);
+                var urlListarContas = `http://localhost:5000/`;
+                request(
+                  {
+                    url: urlListarContas,
+                    method: "GET",
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                  },
+                  function (error, response, body) {
+                    if (!error) {
+                      console.log(body);
+                      const contas = JSON.parse(body);
+                        // var mergedList = _.map(clientes, function(item){
+                        //     return _.extend(item, _.findWhere(contas, { id: item.idCliente }));
+                        // });
+                      const clientesConta = clientes.map(t1 => ({...t1, ...contas.find(t2 => t2.idCliente === t1.id)}))
+                      const jsonBody = {clientesConta}
+                      return res.status(response.statusCode).json(jsonBody);
+                    } else {
+                      console.log("error: " + error);
+                      console.log("response.statusCode: " + response.statusCode);
+                      console.log("response.statusText: " + response.statusText);
+                      return res.status(500).json({"msg": "error"});
+                    }
+                  }
+                );
             } else {
               console.log("error: " + error);
               console.log("response.statusCode: " + response.statusCode);
@@ -44,10 +70,150 @@ module.exports = {
     },
 
     async consultaCliente(req, res) {
-
+        const clienteCpf = req.params.cpf;
+        var urlConsultarClienteCpf = `http://localhost:5001/cpf/${clienteCpf}`;
+        
+        await request(
+          {
+            url: urlConsultarClienteCpf,
+            method: "GET",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          },
+          function (error, response, body) {
+            if (!error) {
+                console.log(body);
+                const cliente = JSON.parse(body);
+                const clienteId = cliente.id;
+                var urlListarContaByIdCliente = `http://localhost:5000/${clienteId}`;
+                request(
+                  {
+                    url: urlListarContaByIdCliente,
+                    method: "GET",
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                  },
+                  function (error, response, body) {
+                    if (!error) {
+                        console.log(body);
+                        const conta = JSON.parse(body);
+                        var urlListarAuthByIdCliente = `http://localhost:5003/usuarios/${clienteId}`;
+                        request(
+                          {
+                            url: urlListarAuthByIdCliente,
+                            method: "GET",
+                            headers: {
+                              'Content-Type': 'application/json'
+                            },
+                          },
+                          function (error, response, body) {
+                            if (!error) {
+                              console.log(body);
+                              const usuario = JSON.parse(body);
+                              const jsonBody = {cliente, usuario, conta}
+                              return res.status(response.statusCode).json(jsonBody);
+                            } else {
+                              console.log("error: " + error);
+                              console.log("response.statusCode: " + response.statusCode);
+                              console.log("response.statusText: " + response.statusText);
+                              return res.status(500).json({"msg": "error"});
+                            }
+                          }
+                        );
+                    } else {
+                      console.log("error: " + error);
+                      console.log("response.statusCode: " + response.statusCode);
+                      console.log("response.statusText: " + response.statusText);
+                      return res.status(500).json({"msg": "error"});
+                    }
+                  }
+                );
+            } else {
+              console.log("error: " + error);
+              console.log("response.statusCode: " + response.statusCode);
+              console.log("response.statusText: " + response.statusText);
+              return res.status(500).json({"msg": "error"});
+            }
+          }
+        );
     },
 
     async consultaTopClientes(req, res) {
+
+        const urlListarClientes = `http://localhost:5001/listar/`;
+
+        console.log(urlListarClientes);
+
+        console.log("aqui");
+        
+        await request(
+          {
+            url: urlListarClientes,
+            method: "GET",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+          },
+          function (error, response, body) {
+            if (!error) {
+                console.log(body);
+                const clientes = JSON.parse(body);
+                const urlListarTopContas = `http://localhost:5000/top/`;
+                request(
+                  {
+                    url: urlListarTopContas,
+                    method: "GET",
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                  },
+                  function (error, response, body) {
+                    if (!error) {
+                        console.log(body);
+                        const contas = JSON.parse(body);
+                        const urlListarUsuarios = `http://localhost:5003/usuarios/`;
+                        request(
+                          {
+                            url: urlListarUsuarios,
+                            method: "GET",
+                            headers: {
+                              'Content-Type': 'application/json'
+                            },
+                          },
+                          function (error, response, body) {
+                            if (!error) {
+                              console.log(body);
+                              const usuarios = JSON.parse(body);
+                              const contasClientes = contas.map(t1 => ({...t1, ...clientes.find(t2 => t2.id === t1.idCliente)}))
+                              const contasTop = contasClientes.map(t1 => ({...t1, ...usuarios.find(t2 => t2.clienteId=== t1.id)}))
+                              const jsonBody = {contasTop}
+                              return res.status(response.statusCode).json(jsonBody);
+                            } else {
+                              console.log("error: " + error);
+                              console.log("response.statusCode: " + response.statusCode);
+                              console.log("response.statusText: " + response.statusText);
+                              return res.status(500).json({"msg": "error"});
+                            }
+                          }
+                        );
+                    } else {
+                      console.log("error: " + error);
+                      console.log("response.statusCode: " + response.statusCode);
+                      console.log("response.statusText: " + response.statusText);
+                      return res.status(500).json({"msg": "error"});
+                    }
+                  }
+                );
+            } else {
+              console.log("error: " + error);
+              console.log("response.statusCode: " + response.statusCode);
+              console.log("response.statusText: " + response.statusText);
+              return res.status(500).json({"msg": "error"});
+            }
+          }
+        );
 
     }
 
--- src/routes/gerenteRouter.js
+++ src/routes/gerenteRouter.js
@@ -5,13 +5,13 @@ const access = require("../middlewares/accessLevel");
 const gerenteController = require("../controllers/gerenteController");
 
 const httpProxy = require("express-http-proxy");
-const gerenteServiceProxy = httpProxy('http://localhost:5002');
 
-gerenteRouter.get("/autocadastro", auth, access.managerAccess, gerenteController.listarAutocadastro);
-gerenteRouter.put("/autocadastro/aprova/:id", auth, access.managerAccess, gerenteController.aprovarAutocadastro);
-gerenteRouter.put("/autocadastro/reprova/:id", auth, access.managerAccess, gerenteController.reprovarAutocadastro);
-gerenteRouter.get("/clientes", auth, access.managerAccess, gerenteController.consultarClientes);
-gerenteRouter.get("/clientes/:id", auth, access.managerAccess, gerenteController.consultaCliente);
-gerenteRouter.get("/clientes/top5", auth, access.managerAccess, gerenteController.consultaTopClientes);
+gerenteRouter.get("/autocadastro", /**auth, access.managerAccess,**/ gerenteController.listarAutocadastro);
+gerenteRouter.put("/autocadastro/aprova/:id", /**auth, access.managerAccess,**/ gerenteController.aprovarAutocadastro);
+gerenteRouter.put("/autocadastro/reprova/:id", /**auth, access.managerAccess,**/ gerenteController.reprovarAutocadastro);
+gerenteRouter.get("/clientes", /**auth, access.managerAccess,**/ gerenteController.consultarClientes);
+gerenteRouter.get("/clientes/:cpf", /**auth, access.managerAccess,**/ gerenteController.consultaCliente);
+gerenteRouter.get("/top", /**auth, access.managerAccess,**/ gerenteController.consultaTopClientes);
 
 module.exports = gerenteRouter;
+
