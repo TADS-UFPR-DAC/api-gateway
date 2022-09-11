e777da918c39698fe642b5220fb3d9fe063e845f
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-09-10 22:23:10 -0300

    Route e controller de admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,32 @@
 var request = require("request");
 
 module.exports = {
-  async listarGerentes(req, res) {},
+  async listarGerentes(req, res) {
+    var urlListarGerentes = `http://localhost:5002/`;
+    console.log(urlListarGerentes);
+
+    await request(
+      {
+        url: urlListarGerentes,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      function (error, response, body) {
+        if (!error) {
+          console.log(body);
+          const jsonBody = JSON.parse(body);
+          return res.status(response.statusCode).json(jsonBody);
+        } else {
+          console.log("error: " + error);
+          console.log("response.statusCode: " + response.statusCode);
+          console.log("response.statusText: " + response.statusText);
+          return res.status(500).json({ msg: "error" });
+        }
+      }
+    );
+  },
 
   async listarGerente(req, res) {
     const gerenteId = req.params.id;
@@ -90,8 +115,8 @@ module.exports = {
             function (error, response, body) {
               if (!error) {
                 console.log(body);
-                const conta = JSON.parse(body);
-                const jsonBody = { cliente, conta, usuario };
+                const usuario = JSON.parse(body);
+                const jsonBody = { gerente, usuario };
                 return res.status(response.statusCode).json(jsonBody);
               } else {
                 console.log("error: " + error);
@@ -111,7 +136,129 @@ module.exports = {
     );
   },
 
-  async editarGerente(req, res) {},
+  async editarGerente(req, res) {
+    id = req.params.id;
+    login = req.body.usuario;
+    senha = req.body.senha;
+    nome = req.body.nome;
+    email = req.body.email;
+    cpf = req.body.cpf;
 
-  async deletarGerente(req, res) {},
+    var urlEditarUsuario = `http://localhost:5003/usuarios/${id}`;
+
+    var urlEditarGerente = `http://localhost:5002/${id}`;
+
+    const sendDataEditarGerente = {
+      cpf: cpf,
+      nome: nome,
+      email: email,
+    };
+    const jsonsendDataEditarGerente = JSON.stringify(sendDataEditarGerente);
+
+    console.log(sendDataEditarGerente);
+    console.log(jsonsendDataEditarGerente);
+
+    await request(
+      {
+        url: urlEditarGerente,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: jsonsendDataEditarGerente,
+      },
+      function (error, response, body) {
+        if (!error) {
+          console.log(body);
+          const gerente = JSON.parse(body);
+          const gerenteId = gerente.id;
+          const sendDataAuthService = {
+            nome: nome,
+            login: login,
+            senha: senha,
+          };
+          const jsonSendDataAuthService = JSON.stringify(sendDataAuthService);
+
+          console.log(sendDataAuthService);
+          request(
+            {
+              url: urlEditarUsuario,
+              method: "PUT",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: jsonSendDataAuthService,
+            },
+            function (error, response, body) {
+              if (!error) {
+                console.log(body);
+                const usuario = JSON.parse(body);
+                const jsonBody = { gerente, usuario };
+                return res.status(response.statusCode).json(jsonBody);
+              } else {
+                console.log("error: " + error);
+                console.log("response.statusCode: " + response.statusCode);
+                console.log("response.statusText: " + response.statusText);
+                return res.status(500).json({ msg: "error" });
+              }
+            }
+          );
+        } else {
+          console.log("error: " + error);
+          console.log("response.statusCode: " + response.statusCode);
+          console.log("response.statusText: " + response.statusText);
+          return res.status(500).json({ msg: "error" });
+        }
+      }
+    );
+  },
+
+  async deletarGerente(req, res) {
+    id = req.params.id;
+
+    var urlDeletarUsuario = `http://localhost:5003/usuarios/${id}`;
+
+    var urlDeletarGerente = `http://localhost:5002/${id}`;
+
+    await request(
+      {
+        url: urlDeletarGerente,
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      function (error, response, body) {
+        if (!error) {
+          console.log(body);
+
+          request(
+            {
+              url: urlDeletarUsuario,
+              method: "DELETE",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            },
+            function (error, response, body) {
+              if (!error) {
+                console.log(body);
+                return res.status(response.statusCode).json(jsonBody);
+              } else {
+                console.log("error: " + error);
+                console.log("response.statusCode: " + response.statusCode);
+                console.log("response.statusText: " + response.statusText);
+                return res.status(500).json({ msg: "error" });
+              }
+            }
+          );
+        } else {
+          console.log("error: " + error);
+          console.log("response.statusCode: " + response.statusCode);
+          console.log("response.statusText: " + response.statusText);
+          return res.status(500).json({ msg: "error" });
+        }
+      }
+    );
+  },
 };
